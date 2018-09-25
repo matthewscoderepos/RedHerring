e35a92aea681242b416e480e8d60bb5c65ea08b5
Matthew <matthew.sutton09@gmail.com>
2018-09-25 16:23:08 -0400

    Update main.asm

--- main.asm
+++ main.asm
@@ -12,6 +12,7 @@
  methodQuestion: .asciiz "What encoding method would you like to use? "
  rbgQuestion: .asciiz "Red, Green, or Blue? "
  fileLocation: .asciiz "Where is the picture located? "
+ hiddenMessage .asciiz "Please provide the message to be hidden in the picture: "
 
 .text
 #text
