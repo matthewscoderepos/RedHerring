c1c10a53cbb6e5f7ff365ac711e16918ab097e1b
Matthew  Sutton <matthew.sutton09@gmail.com>
2018-09-25 15:48:12 -0400

    Added questions

--- main.asm
+++ main.asm
@@ -8,7 +8,9 @@
 ##############################
 
 .data
-#data
+ keyQuestion: .asciiz "What is the key? "
+ methodQuestion: .asciiz "What encoding method would you like to use? "
+ rbgQuestion: .asciiz "Red, Green, or Blue? "
 
 .text
 #text
