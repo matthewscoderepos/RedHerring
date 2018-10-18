45146cd8778a7b7dd2ad3c89f63816380bb7994a
ccrino <ccrino@mail.usf.edu>
2018-10-18 11:49:41 -0400

    added memory, file, and rand macros

--- Macros.asm
+++ Macros.asm
@@ -55,4 +55,61 @@ li $v0, 36
 syscall
 .end_macro
 
+.macro malloc (%size, %form)
+li $v0, 9
+%form ($a0, %size)
+syscall
+.end_macro
+
+.eqv	READ 0
+.eqv   WRITE 1
+.macro openf (%filename, %form, %flag)
+li $v0, 13
+%form ($a0, %filename)
+li $a1, %flag
+li $a2, 0
+syscall
+.end_macro
+
+.macro readf (%fileptr, %form1, %bufferptr, %form2, %readcnt, %form3)
+li $v0, 14
+%form1 ($a0, %fileptr)
+%form2 ($a1, %bufferptr)
+%form3 ($a2, %readcnt)
+syscall
+.end_macro
+
+.macro writef (%fileptr, %form1, %bufferptr, %form2, %writecnt, %form3)
+li $v0, 15
+%form1 ($a0, %fileptr)
+%form2 ($a1, %bufferptr)
+%form3 ($a2, %writecnt)
+syscall
+.end_macro
+
+.macro closef (%fileptr, %form1)
+li $v0, 16
+%form1 ($a0, %fileptr)
+syscall
+.end_macro
+
+.macro seedrand (%randid, %form1, %randseed, %form2)
+li $v0, 40
+%form1 ($a0, %randid)
+%form2 ($a1, %randseed)
+syscall
+.end_macro
+
+.macro randomIntRange (%randid, %form1, %upperbound, %form2)
+li $v0, 42
+%form1 ($a0, %randid)
+%form2 ($a1, %upperbound)
+syscall
+.end_macro
+
+
+
+
+
+
 
