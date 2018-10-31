ac2e98ad14f97dc56712881a2831ad3618f0557f
ccrino <ccrino@mail.usf.edu>
2018-10-31 10:27:14 -0400

    updated main
    included macros
    added header access, and pixel map isolation

--- main.asm
+++ main.asm
@@ -10,9 +10,7 @@
 #$S1 holds new file name. 
 #$S2 holds original picture data
 
-
-###### WE WILL NEED TO LOAD THE HEADER SEPERATELY TO GATHER ITS INFO. THE PICTURE LOADING AND COPYING WAS A LOT HARDER THAN I THOUGHT. #########
-
+.include "Macros.asm"
 
 .data
  keyQuestion: .asciiz "What is the key? "
@@ -22,9 +20,8 @@
  showFileLocation: .asciiz "You entered this file location:\n"
  hiddenMessage: .asciiz "Please provide the message to be hidden in the picture: "
  newFileName: .asciiz "Please provide a name for the new picture: "
- finString: .ascii "                                                                                                     "
- foutString: .ascii "                                                                                                     "
- pictureBuffer: .space 150000
+ finString: .space 100
+ foutString: .space 100
  
 .text
 main:
@@ -32,68 +29,77 @@ main:
 userInputFileLocation:
 	# $s0 holds file location
 
-	li $v0, 4
-	la $a0, fileLocation 	#Ask user string
-	syscall
+	tell (fileLocation, adr) 	#Ask user string
 	
-	li $v0, 8 		#User input of picture location
-	la $a0, finString
-	li $a1, 100 		#hardcoded max size of string
-	move $s0, $a0 		#Moving file location into s0
-	syscall
+	la $s0, finString		#pointing s0 to finstring
+	listen ($s0, reg, 100, imd)	#reading user input
 	
 	
 	#Full disclosure, I got this loop from StackOverflow. We need to strip the newline chars that are added to inputs and this does the job.
 	xor $a2, $a2, $a2
 	loop:
-    lbu $a3, finString($a2)  
-    addiu $a2, $a2, 1
-    bnez $a3, loop      	  # Search the NULL char code
+	lbu $a3, finString($a2)  
+	addiu $a2, $a2, 1
+	bnez $a3, loop      	  # Search the NULL char code
    	beq $a1, $a2, skip   	  # Check whether the buffer was fully loaded
-    subiu $a2, $a2, 2   	  # Otherwise 'remove' the last character
-    sb $0, finString($a2)     # and put a NULL instead
+	subiu $a2, $a2, 2   	  # Otherwise 'remove' the last character
+	sb $0, finString($a2)     # and put a NULL instead
 	skip:
 
 readPicture:
 	# $s2 holds old picture data
-	#open file
-	li   $v0, 13       	 # system call for open file
-	la   $a0, finString      # board file name
-	li   $a1, 0       	 # Open for reading
-	li   $a2, 0
-	syscall           	 # open a file (file descriptor returned in $v0)
-	move $s2, $v0     	 # save the file descriptor 
+	#open file 
+	openf ($s0, reg, READ)
+	move $s2, $v0
+	
+	#allocate 56 bytes (header min size 54, padded by 2 for word align)
+	malloc (56, imd)
+	move $s3, $v0	# save allocated memory pointer
+	addi $s3, $s3, 2	# ignore padding bytes
 
+	
 	#read from file
-	li   $v0, 14     	 # system call for read from file
-	move $a0, $s2     	 # file descriptor 
-	la   $a1, pictureBuffer  # address of buffer to which to read
-	li   $a2, 150000    	 #hardcoded, I belive that the size of the picture is in the header somewhere, so if we pull that out we can not hardcode here.
-	syscall            	 # read from file
-
+	readf ($s2, reg, $s3, reg, 54, imd)
+	
+	#input validation
+	lhu $t1, 0($s3)				# load a half
+	li $t0,	0x00004d42			# load hex value of BM, remember edianess 
+	bne $t1, $t0, exit
+	
+	lhu $t0, 28($s3)			# check for color depth 24
+	li $t1, 24
+	bne $t1, $t0, exit			# if not, abort
+	
+	#calculate pixel map size
+	lw $t0, 2($s3) 				#load bytes in file
+	lw $t1, 10($s3)				#load bytes to pixel map
+	sub $s6, $t0, $t1			# bytes in file - bytes to map = bytes in map
+	
+	# allocate space for map load
+	malloc ($s6, reg)
+	move $s7, $v0
+	
+	#read from file
+	readf ($s2, reg, $s7, reg, $s6, reg)	#pixel map now in memory
+	
 	# Close the file 
-	li   $v0, 16      	 # system call for close file
-	move $a0, $s2     	 # file descriptor to close
-	syscall            	 # close file	
-
-
+	closef ($s2, reg)
+	
+	# at this point we'll need the message to start encoding
+	
+	
 userInputNewFileName:
 	# $s1 holds new file name
 	
-	li $v0, 4
-	la $a0, newFileName 	#Ask user string
-	syscall
+	tell (newFileName, adr)
 	
-	li $v0, 8		#User input of new name
-	la $a0, foutString
-	li $a1, 100		#hardcoded max size of string
-	move $s1, $a0		#moving new name into s1
-	syscall
+	la $s1, foutString
+	listen ($s1, reg, 100, imd)
 	
 	#Full disclosure, I got this loop from StackOverflow. We need to strip the newline chars that are added to inputs and this does the job.
 	xor $a2, $a2, $a2
 	loop2:
-    lbu $a3, foutString($a2)  
+	lbu $a3, foutString($a2)  
    	addiu $a2, $a2, 1
    	bnez $a3, loop2		# Search the NULL char code
    	beq $a1, $a2, skip2   	# Check whether the buffer was fully loaded
@@ -104,21 +110,13 @@ userInputNewFileName:
 	
 openNewFile:
 	#opens a new file with name given by user for writing
-	li $v0, 13
-	la $a0, foutString
-	li $a1, 1
-	syscall
+	openf (foutString, adr, WRITE)
 	move $s2, $v0
-	#begin writing from pictureBuffer
-	li $v0, 15
-	move $a0, $s2
-	la $a1, pictureBuffer
-	li $a2, 150000 		#hardcoded, I belive that the size of the picture is in the header somewhere, so if we pull that out we can not hardcode here.
-	syscall
 	
-	li $v0, 16
-	move $a0, $s2
-	syscall
+	#begin writing from pictureBuffer
+	writef ($s2, reg, $s3, reg, 54, imd)		#copy forward header
+	writef ($s2, reg, $s7, reg, $s6, reg)		#copy in edited pixel map
+	closef ($s2, reg)
 	
 exit:
 	li $v0, 10
