b59ba05cbf9937e21db6a6e9ebf4264f037ff0f6
ccrino <ccrino@mail.usf.edu>
2018-11-14 09:46:01 -0500

    en/decode methods in seperate files
    simple LSB encode/decode in simpleLSB.asm etc.
    RandomMonoLSB renamed KeyedMonoLSB

---
+++ monoLSB.asm
@@ -0,0 +1,190 @@
+# # # # # # # # # # # # #
+#     Monochrome LSB	#
+#			#
+#   			#
+# # # # # # # # # # # # #
+
+.text
+EnMonoLSB:
+	tell(chooseColor, adr)
+	readc()
+	li $t0, 0x52
+	beq $v0, $t0, R 
+	li $t0, 0x47
+	beq $v0, $t0, G
+	li $t0, 0x42
+	beq $v0, $t0, B
+
+
+	R:
+	addi $t3, $0, 0 #R is the 0th byte, skip 0
+	j endRGB
+	G:
+	addi $t3, $0, 1 #G is the 2nd byte, skip 1
+	j endRGB
+	B:
+	addi $t3, $0, 2 #B is the 3rd byte, skip 2
+	
+    endRGB:
+
+	lw $s5, 18($s2)					# gets the width of pixel map in pixels
+	mul $s5, $s5, 3					# width of pixel map in bytes
+	move $s6, $s5					# copy of width
+	andi $t1, $s5, 0x00000003		# check if last 2 bits are not zero
+	beqz $t1, EMLSB_aligned			# this evalutates false if and only if the byte count is not divisible by 4
+	andi $s6, $s6, 0xfffffffc		# so then generated an adjusted width add padding until
+	addi $s6, $s6, 4				# it is divisible by 4
+	EMLSB_aligned:
+	lw $s7, 22($s2)					# gets the height of pixel map in pixels
+	slt $t0, $s7, $0
+	beqz $t0, EMLSB_posHeight
+	not $s7, $s7
+	addi $s7, $s7, 1
+	EMLSB_posHeight:
+	#Sets up the pixel array so that we can work on it. Pads width, sets the height to be positive
+
+	move $t5, $0					# ending flag
+	move $t7, $t3					# width index
+	move $t8, $0					# height index
+	#Sets t5, t7, and t8 to 0, all used for determining where we are in the pixel array
+
+	EMLSB_nextByte:
+	lbu $t0, 0($s4)					# get next message byte
+	bnez $t5, EMLSB_Exit
+	bnez $t0, EMLSB_Cont			# check if message is fully encoded yet
+	addi $t5, $t5, 1
+	#Loads 8 bits (1 byte) of the message into t0. If this fails and t0 is set to 0, t5 is set to 1 and the loop will exit on the next iteration 
+
+	EMLSB_Cont:
+	addi $s4, $s4, 1
+	li $t9, 8						# message byte index, 8 bits left to encode
+	#Moves the message pointer 1 byte forward because we loaded the byte already in the last loop, sets t9 to 8 (used to determine if we need to load another byte of message or not)
+
+
+	EMLSB_nextBit:
+	andi $t1, $t0, 1				# bit of message to append
+	mult $t8, $s6					# 
+	mflo $t6						#
+	addu $t6, $t6, $t7				#
+	addu $t6, $t6, $s3				# address of next channel to edit
+	lbu $t2, 0($t6)					#loads the byte that we are changing into t2
+	andi $t2, $t2, 0xfe				# discard LSB
+	or $t2, $t2, $t1				# replace with message
+	sb $t2, 0($t6)					#stores the byte in question into t6
+	#Determines where in the pixel array we are at, loads the byte we are at and edits that byte to contain our message data
+
+
+	add $t7, $t7, 3					# increment width index
+	bne $t7, $s5, EMLSB_wnRange		# check its still within range
+	add $t8, $t8, 1					# if not increment height index
+	beq $t8, $s7, EMLSB_imageEnd	# check if height within range
+	move $t7, $t3					# reset width index
+	#Determines if we are at the end of a line, if we are it resets the width index and moves us down a row. If we drop off the end of the pixel array we are out of image data
+
+	EMLSB_wnRange:
+	addi $t9, $t9, -1				# decrement byte index
+	beqz $t9, EMLSB_nextByte		# if 0 then get next byte else
+	srl $t0, $t0, 1
+	j EMLSB_nextBit					# get next bit
+	#Determines if we still have message data in our loaded byte, if we do not it loads a new byte and if we do it starts the message encode on the next bit
+
+	EMLSB_imageEnd:
+	tell (ImageEndOfFileE, adr)
+	EMLSB_Exit:
+	jr $ra
+	
+
+DeMonoLSB:
+	tell(rgbQuestion, adr)
+	readc()
+	li $t0, 0x52
+	beq $v0, $t0, DR 
+	li $t0, 0x47
+	beq $v0, $t0, DG
+	li $t0, 0x42
+	beq $v0, $t0, DB
+
+
+	DR:
+	addi $t3, $0, 0 #R is the 0th byte, skip 0
+	j dEndRGB
+	DG:
+	addi $t3, $0, 1 #G is the 2nd byte, skip 1
+	j dEndRGB
+	DB:
+	addi $t3, $0, 2 #B is the 3rd byte, skip 2
+	
+	dEndRGB:
+
+
+	lw $t0, -4($s3)					# get map size
+	div $t0, $t0, 8					# approximate max message size
+	addi $t0, $t0, 1				# pad 1
+	malloc ($t0, reg)				# 
+	move $s4, $v0					# move to message pointer
+	#allocates space for the encoded message
+	
+	lw $s5, 18($s2)					# gets the width of pixel map in pixels
+	li $t1, 3						# 3 bytes per pixel
+	mult $s5, $t1
+	mflo $s5						# width of pixel map in bytes
+	move $s6, $s5					# copy of width
+	andi $t1, $s5, 0x00000003		# check if last 2 bits are not zero
+	beqz $t1, DMLSB_aligned			# this evalutates true if and only if the byte count is not divisible by 4
+	andi $s6, $s6, 0xfffffffc		# so then generated an adjusted width add padding until
+	addi $s6, $s6, 4				# it is divisible by 4
+	DMLSB_aligned:
+	lw $s7, 22($s2)					# gets the height of pixel map in pixels
+	slt $t0, $s7, $0
+	beqz $t0, DMLSB_posHeight
+	not $s7, $s7
+	addi $s7, $s7, 1
+	DMLSB_posHeight:
+	#Sets up the pixel array so that we can work on it. Pads width, sets the height to be positive
+
+	move $t5, $s4					# message index
+	move $t7, $t3					# width index
+	move $t8, $0					# height index
+	j DMLSB_skipIn
+	#Sets t5 to the message pointer
+
+	DMLSB_nextByte: 
+	srl $t0, $t0, 1
+	sb $t0, 0($t5)
+	beqz $t0, DMLSB_Exit
+	add $t5, $t5, 1
+	#shifts t0 to the left, stores t0 in t5 (the message), if t0 = 0, were done
+
+	DMLSB_skipIn:
+	move $t0, $0					# reset message byte
+	li $t9, 8						# message byte index, 8 bits left to decode
+	#t0 = 0, bits we need to read = 8
+
+	DMLSB_nextBit:
+	mul $t6, $t8, $s6				# 
+	addu $t6, $t6, $t7				#
+	addu $t6, $t6, $s3				# address of next channel to read
+	lbu $t1, 0($t6)					#loads a byte from t6 into t1
+	andi $t1, $t1, 1				# discard everything but LSB
+	sll $t1, $t1, 8					#shift t1 left 8 bits
+	add $t0, $t0, $t1				# append LSB
+	
+	#checkBounds
+	add $t7, $t7, 3					# increment width index
+	bne $t7, $s5, DMLSB_wnRange		# check its still within range
+	add $t8, $t8, 1					# if not increment height index
+	beq $t8, $s7, DMLSB_imageEnd	# check if height within range
+	move $t7, $t3						# reset width index
+	#Checking bounds, resets width and increases height when needed, if we drop off the image we end
+
+	DMLSB_wnRange:
+	addi $t9, $t9, -1				# decrement byte index
+	beqz $t9, DMLSB_nextByte		# if 0 then get next byte else
+	srl $t0, $t0, 1
+	j DMLSB_nextBit					# get next bit
+	#if we have bits left to decode get the next bit, otherwise get the next byte 
+
+	DMLSB_imageEnd:
+	tell (ImageEndOfFileD, adr)
+	DMLSB_Exit:
+	jr $ra
---
+++ simpleLSB.asm
@@ -0,0 +1,152 @@
+# # # # # # # # # # # # #
+#	Simple LSB	#
+#			#
+#   			#
+# # # # # # # # # # # # #
+
+.text
+EnSimpleLSB:
+	
+	
+	lw $s5, 18($s2)					# gets the width of pixel map in pixels
+	mul $s5, $s5, 3					# width of pixel map in bytes
+	move $s6, $s5					# copy of width
+	andi $t1, $s5, 0x00000003		# check if last 2 bits are not zero
+	beqz $t1, ESLSB_aligned			# this evalutates false if and only if the byte count is not divisible by 4
+	andi $s6, $s6, 0xfffffffc		# so then generated an adjusted width add padding until
+	addi $s6, $s6, 4				# it is divisible by 4
+	ESLSB_aligned:
+	lw $s7, 22($s2)					# gets the height of pixel map in pixels
+	slt $t0, $s7, $0
+	beqz $t0, ESLSB_posHeight
+	not $s7, $s7
+	addi $s7, $s7, 1
+	ESLSB_posHeight:
+	#Sets up the pixel array so that we can work on it. Pads width, sets the height to be positive
+
+	move $t5, $0					# ending flag
+	move $t7, $0					# width index
+	move $t8, $0					# height index
+	#Sets t5, t7, and t8 to 0, all used for determining where we are in the pixel array
+
+	ESLSB_nextByte:
+	lbu $t0, 0($s4)					# get next message byte
+	bnez $t5, ESLSB_Exit
+	bnez $t0, ESLSB_Cont			# check if message is fully encoded yet
+	addi $t5, $t5, 1
+	#Loads 8 bits (1 byte) of the message into t0. If this fails and t0 is set to 0, t5 is set to 1 and the loop will exit on the next iteration 
+
+	ESLSB_Cont:
+	addi $s4, $s4, 1
+	li $t9, 8						# message byte index, 8 bits left to encode
+	#Moves the message pointer 1 byte forward because we loaded the byte already in the last loop, sets t9 to 8 (used to determine if we need to load another byte of message or not)
+
+
+	ESLSB_nextBit:
+	andi $t1, $t0, 1				# bit of message to append
+	mult $t8, $s6					# 
+	mflo $t6						#
+	addu $t6, $t6, $t7				#
+	addu $t6, $t6, $s3				# address of next channel to edit
+	lbu $t2, 0($t6)					#loads the byte that we are changing into t2
+	andi $t2, $t2, 0xfe				# discard LSB
+	or $t2, $t2, $t1				# replace with message
+	sb $t2, 0($t6)					#stores the byte in question into t6
+	#Determines where in the pixel array we are at, loads the byte we are at and edits that byte to contain our message data
+
+
+	add $t7, $t7, 1					# increment width index
+	bne $t7, $s5, ESLSB_wnRange		# check its still within range
+	add $t8, $t8, 1					# if not increment height index
+	beq $t8, $s7, ESLSB_imageEnd	# check if height within range
+	move $t7, $0					# reset width index
+	#Determines if we are at the end of a line, if we are it resets the width index and moves us down a row. If we drop off the end of the pixel array we are out of image data
+
+	ESLSB_wnRange:
+	addi $t9, $t9, -1				# decrement byte index
+	beqz $t9, ESLSB_nextByte		# if 0 then get next byte else
+	srl $t0, $t0, 1
+	j ESLSB_nextBit					# get next bit
+	#Determines if we still have message data in our loaded byte, if we do not it loads a new byte and if we do it starts the message encode on the next bit
+
+	ESLSB_imageEnd:
+	tell (ImageEndOfFileE, adr)
+	ESLSB_Exit:
+	jr $ra
+	
+	
+	
+	
+DeSimpleLSB:
+	
+	lw $t0, -4($s3)					# get map size
+	div $t0, $t0, 8					# approximate max message size
+	addi $t0, $t0, 1				# pad 1
+	malloc ($t0, reg)				# 
+	move $s4, $v0					# move to message pointer
+	#allocates space for the encoded message
+	
+	lw $s5, 18($s2)					# gets the width of pixel map in pixels
+	li $t1, 3						# 3 bytes per pixel
+	mult $s5, $t1
+	mflo $s5						# width of pixel map in bytes
+	move $s6, $s5					# copy of width
+	andi $t1, $s5, 0x00000003		# check if last 2 bits are not zero
+	beqz $t1, DSLSB_aligned			# this evalutates true if and only if the byte count is not divisible by 4
+	andi $s6, $s6, 0xfffffffc		# so then generated an adjusted width add padding until
+	addi $s6, $s6, 4				# it is divisible by 4
+	DSLSB_aligned:
+	lw $s7, 22($s2)					# gets the height of pixel map in pixels
+	slt $t0, $s7, $0
+	beqz $t0, DSLSB_posHeight
+	not $s7, $s7
+	addi $s7, $s7, 1
+	DSLSB_posHeight:
+	#Sets up the pixel array so that we can work on it. Pads width, sets the height to be positive
+
+	move $t5, $s4					# message index
+	move $t7, $0					# width index
+	move $t8, $0					# height index
+	j DSLSB_skipIn
+	#Sets t5 to the message pointer
+
+	DSLSB_nextByte: 
+	srl $t0, $t0, 1
+	sb $t0, 0($t5)
+	beqz $t0, DSLSB_Exit
+	add $t5, $t5, 1
+	#shifts t0 to the left, stores t0 in t5 (the message), if t0 = 0, were done
+
+	DSLSB_skipIn:
+	move $t0, $0					# reset message byte
+	li $t9, 8						# message byte index, 8 bits left to decode
+	#t0 = 0, bits we need to read = 8
+
+	DSLSB_nextBit:
+	mul $t6, $t8, $s6				# 
+	addu $t6, $t6, $t7				#
+	addu $t6, $t6, $s3				# address of next channel to read
+	lbu $t1, 0($t6)					#loads a byte from t6 into t1
+	andi $t1, $t1, 1				# discard everything but LSB
+	sll $t1, $t1, 8					#shift t1 left 8 bits
+	add $t0, $t0, $t1				# append LSB
+	
+	#checkBounds
+	add $t7, $t7, 1					# increment width index
+	bne $t7, $s5, DSLSB_wnRange		# check its still within range
+	add $t8, $t8, 1					# if not increment height index
+	beq $t8, $s7, DSLSB_imageEnd	# check if height within range
+	li $t7, 0						# reset width index
+	#Checking bounds, resets width and increases height when needed, if we drop off the image we end
+
+	DSLSB_wnRange:
+	addi $t9, $t9, -1				# decrement byte index
+	beqz $t9, DSLSB_nextByte		# if 0 then get next byte else
+	srl $t0, $t0, 1
+	j DSLSB_nextBit					# get next bit
+	#if we have bits left to decode get the next bit, otherwise get the next byte 
+
+	DSLSB_imageEnd:
+	tell (ImageEndOfFileD, adr)
+	DSLSB_Exit:
+	jr $ra
